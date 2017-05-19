3e16045a007ab80946c52c6df68e94a275b2fd8c
Nicklas Andersson <nicklas.andersson@leanon.se>
2017-05-19 16:53:34 +0200

    Automaticly download data from api

--- src/main/java/se/starkt/service/ArtiklarService.java
+++ src/main/java/se/starkt/service/ArtiklarService.java
@@ -1,15 +1,20 @@
 package se.starkt.service;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
+import lombok.extern.apachecommons.CommonsLog;
 import model.Artikel;
 import org.springframework.stereotype.Service;
 import se.starkt.ArtiklarParser;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import java.util.ArrayList;
 
 /**
  * Created by
@@ -17,8 +22,10 @@ import java.util.ArrayList;
  * @author nicklas on 2017-03-29.
  */
 @Service
+@CommonsLog
 public class ArtiklarService {
 
+    final static int size = 1024;
     /**
      * The file usually contains ~17000 items
      */
@@ -31,6 +38,7 @@ public class ArtiklarService {
     String skapadTid;
 
     public ArtiklarService() {
+        SimpleDateFormat sdp = new SimpleDateFormat("yyyyMMdd'T'HHmmssS");
         ArtiklarParser parser = null;
         try {
 
@@ -39,7 +47,35 @@ public class ArtiklarService {
 
             parser = new ArtiklarParser();
 
-            saxParser.parse("20170323.xml", parser);
+
+            String fAddress = "https://www.systembolaget.se/api/assortment/products/xml";
+
+            URLConnection uCon = null;
+
+            InputStream is = null;
+            try {
+                URL url;
+                byte[] buf;
+                int byteRead, byteWritten = 0;
+                url = new URL(fAddress);
+
+
+                uCon = url.openConnection();
+                is = uCon.getInputStream();
+                //buf = new byte[size];
+
+                saxParser.parse(is, parser);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
 
         } catch (Exception e) {
             e.printStackTrace();
