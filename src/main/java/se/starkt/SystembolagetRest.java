31c0b56953bc8d84c1fe3852d27c9281b39f5ff6
nicklas <nicklas.andersson@leanon.se>
2018-09-12 16:33:10 +0200

    Cleanup

--- src/main/java/se/starkt/SystembolagetRest.java
+++ src/main/java/se/starkt/SystembolagetRest.java
@@ -1,26 +1,14 @@
 package se.starkt;
 
-import model.ArtikelDao;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EntityScan(basePackageClasses = ArtikelDao.class)
-public class SystembolagetRest extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(
-            SpringApplicationBuilder application) {
-        return application.sources(SystembolagetRest.class);
+public class SystembolagetRest {
+    public static void main(String[] args) {
+        SpringApplication.run(SystembolagetRest.class, args);
     }
-
-    public static void main(String argv[]) {
-        SpringApplication.run(SystembolagetRest.class, argv);
-    }
-
 }
